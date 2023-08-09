576845c3cf9540a6be8091302ecb2f2d6ae97c74
kartik81687 <106053449+kartik81687@users.noreply.github.com>
2023-08-09 09:33:13 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,32 +9,30 @@ export default function HomePage() {
     <div className="flex max-w-6xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 sm:mt-20 mt-20 background-gradient">
-
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-bold tracking-normal text-gray-300 sm:text-7xl">
-          Generating dream rooms{" "}
+          利用人工智能生成梦想空间{" "}
           <span className="relative whitespace-nowrap text-blue-600">
             <SquigglyLines />
-            <span className="relative">using AI</span>
+            <span className="relative">使用 AI</span>
           </span>{" "}
-          for everyone.
+          为每个人。
         </h1>
-        <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400  text-gray-500 leading-7">
-          Take a picture of your room and see how your room looks in different
-          themes. 100% free – remodel your room today.
+        <h2 className="mx-auto mt-12 max-w-xl text-lg sm:text-gray-400 text-gray-500 leading-7">
+          拍摄您的房间照片，看看不同主题下的房间效果。100% 免费 - 今天就重新布置您的房间。
         </h2>
         <Link
           className="bg-blue-600 rounded-xl text-white font-medium px-4 py-3 sm:mt-10 mt-8 hover:bg-blue-500 transition"
           href="/dream"
         >
-          Generate your dream room
+          生成您的梦想房间
         </Link>
         <div className="flex justify-between items-center w-full flex-col sm:mt-10 mt-6">
           <div className="flex flex-col space-y-10 mt-4 mb-16">
             <div className="flex sm:space-x-8 sm:flex-row flex-col">
               <div>
-                <h3 className="mb-1 font-medium text-lg">Original Room</h3>
+                <h3 className="mb-1 font-medium text-lg">原始房间</h3>
                 <Image
-                  alt="Original photo of a room with roomGPT.io"
+                  alt="房间照片（原始图像）"
                   src="/original-pic.jpg"
                   className="w-full object-cover h-96 rounded-2xl"
                   width={400}
@@ -42,9 +40,9 @@ export default function HomePage() {
                 />
               </div>
               <div className="sm:mt-0 mt-8">
-                <h3 className="mb-1 font-medium text-lg">Generated Room</h3>
+                <h3 className="mb-1 font-medium text-lg">生成的房间</h3>
                 <Image
-                  alt="Generated photo of a room with roomGPT.io"
+                  alt="房间照片（生成图像）"
                   width={400}
                   height={400}
                   src="/generated-pic-2.jpg"
